1b18251e5d8444e8d7a4994643851ba67178ec94
nederht <t.nederhoff@rn.rabobank.nl>
2015-09-05 14:58:23 +0200

    second commit

--- src/main/java/GiroReader.java
+++ src/main/java/GiroReader.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class GiroReader {
+    //test
     public static void main(String[] args) {
         try {
             String filePath = "/home/tim/Documents/giroMutaties/15-07-2015_15-08-2015.csv", line, mutatie[], colString ="", header[];
